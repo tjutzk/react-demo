fc13c9111253a49292d57a5723eb244fd2087ff2
tjutzk <1356476048@qq.com>
2019-03-28 13:20:17 +0800

    首次上传

---
+++ src/comment/CommentApp.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class CommentApp extends Component{
+    render(){
+        return (
+            <div>CommentApp</div>
+        );
+    }
+}
+
+export default CommentApp
