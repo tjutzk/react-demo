245996074db459546c7f8719a6b6bb7660032a9a
tjutzk <1356476048@qq.com>
2019-04-11 17:26:12 +0800

    改为使用react-redux重构

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import CommentApp from './comment/CommentApp'
+import CommentApp from './containers/CommentApp'
 import './App.css';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comment'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(commentsReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <CommentApp /> 
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
